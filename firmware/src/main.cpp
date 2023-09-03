2b987d08e518b1a121c4ce5ce5d1451c84ba542a
Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
2023-09-03 17:25:00 +0200

    add mqtt

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -4,6 +4,8 @@
 #include <AsyncElegantOTA.h>
 #include <SPIFFS.h>
 
+#include <espMqttClient.h>
+
 #include "esp_system.h"
 #include "esp_log.h"
 #include "esp_err.h"
@@ -20,18 +22,30 @@
 AsyncWebServer server(80);
 
 //Variables to save values from HTML form
-String ssid;
-String pass;
 String aeskey;
 String smartmeter_html;
+String mqttbroker;
+int    mqttport;
+String mqtttopic;
+String mqtt_html;
+String ssid;
+String pass;
+String wlan_html;
 
 // File paths to save input values permanently
 const char* ssidPath = "/ssid.txt";
 const char* passPath = "/pass.txt";
 const char* aeskeyPath = "/aeskey.txt";
+const char* mqttbrokerPath = "/mqttbroker.txt";
+const char* mqttportPath = "/mqttport.txt";
+const char* mqtttopicPath = "/mqtttopic.txt";
 
 unsigned long previousMillis = 0;
 const long interval = 10000;  // interval to wait for Wi-Fi connection (milliseconds)
+
+espMqttClient mqttClient;
+bool reconnectMqtt = false;
+uint32_t lastReconnect = 0;
 
 // Set LED GPIO
 const int ledPin = 37;
@@ -115,6 +129,10 @@ uint32_t pplus;
 uint32_t pminus;
 uint32_t aplus;
 uint32_t aminus;
+uint32_t qplus;
+uint32_t qminus;
+uint32_t rplus;
+uint32_t rminus;
 
 static bool handle_rx(const uint8_t *data, size_t data_len, void *arg)
 {
@@ -154,13 +172,31 @@ static bool handle_rx(const uint8_t *data, size_t data_len, void *arg)
 
             pplus =  (uint32_t)pay_load[44] | ((uint32_t)pay_load[45] << 8) | ((uint32_t)pay_load[46] << 16) | ((uint32_t)pay_load[47] << 24);
             pminus = (uint32_t)pay_load[51] | ((uint32_t)pay_load[52] << 8) | ((uint32_t)pay_load[53] << 16) | ((uint32_t)pay_load[54] << 24);
+            rplus =  (uint32_t)pay_load[58] | ((uint32_t)pay_load[59] << 8) | ((uint32_t)pay_load[60] << 16) | ((uint32_t)pay_load[61] << 24);
+            rminus = (uint32_t)pay_load[66] | ((uint32_t)pay_load[67] << 8) | ((uint32_t)pay_load[68] << 16) | ((uint32_t)pay_load[69] << 24);
             aplus =  (uint32_t)pay_load[12] | ((uint32_t)pay_load[13] << 8) | ((uint32_t)pay_load[14] << 16) | ((uint32_t)pay_load[15] << 24);
             aminus = (uint32_t)pay_load[19] | ((uint32_t)pay_load[20] << 8) | ((uint32_t)pay_load[21] << 16) | ((uint32_t)pay_load[22] << 24);
+            qplus =  (uint32_t)pay_load[28] | ((uint32_t)pay_load[29] << 8) | ((uint32_t)pay_load[30] << 16) | ((uint32_t)pay_load[31] << 24);
+            qminus = (uint32_t)pay_load[38] | ((uint32_t)pay_load[39] << 8) | ((uint32_t)pay_load[40] << 16) | ((uint32_t)pay_load[41] << 24);
 
             ESP_LOGD(TAG, "P+: %d W\n", pplus);
             ESP_LOGD(TAG, "P-: %d W\n", pminus);
             ESP_LOGD(TAG, "A+: %d Wh\n", aplus);
             ESP_LOGD(TAG, "A-: %d Wh\n", aminus);
+            ESP_LOGD(TAG, "Q+: %d var\n", qplus);
+            ESP_LOGD(TAG, "Q-: %d var\n", qminus);
+            ESP_LOGD(TAG, "R+: %d varh\n", rplus);
+            ESP_LOGD(TAG, "R-: %d varh\n", rminus);
+
+            char message[256];
+
+            snprintf(message, sizeof(message), 
+            "{\"counter\":{\"A+\":%d,\"A-\":%d,\"R+\":%d,\"R-\":%d},\"power\":{\"P+\":%d,\"P-\":%d,\"Q+\":%d,\"Q-\":%d}}",
+            aplus, aminus, rplus, rminus, pplus, pminus, qplus, qminus);
+
+            if (mqttClient.connected()) {
+                mqttClient.publish(mqtttopic.c_str(), 1, false, message);
+            }
         } else {
             in_sync = false;
              ESP_LOGE(TAG, "checksum error\n");
@@ -201,7 +237,7 @@ static void handle_event_ftd232(const ftd232_host_dev_event_data_t *event, void 
     }
 }
 
-static void main_task(void *arg) {
+static void usb_task(void *arg) {
 
     esp_err_t err;
     BaseType_t task_created;
@@ -305,24 +341,24 @@ bool initWiFi() {
         return false;
     }
 
-  WiFi.mode(WIFI_STA);
+    WiFi.mode(WIFI_STA);
 
-  WiFi.begin(ssid.c_str(), pass.c_str());
-  Serial.println("Connecting to WiFi...");
+    WiFi.begin(ssid.c_str(), pass.c_str());
+    Serial.println("Connecting to WiFi...");
 
-  unsigned long currentMillis = millis();
-  previousMillis = currentMillis;
+    unsigned long currentMillis = millis();
+    previousMillis = currentMillis;
 
-  while(WiFi.status() != WL_CONNECTED) {
-    currentMillis = millis();
-    if (currentMillis - previousMillis >= interval) {
-      Serial.println("Failed to connect.");
-      return false;
-    }
-  }
+    while(WiFi.status() != WL_CONNECTED) {
+        currentMillis = millis();
+        if (currentMillis - previousMillis >= interval) {
+            Serial.println("Failed to connect.");
+            return false;
+        }
+     }
 
-  Serial.println(WiFi.localIP());
-  return true;
+     Serial.println(WiFi.localIP());
+     return true;
 }
 
 static void Set_key(String aeskey) {
@@ -338,12 +374,59 @@ static void Set_key(String aeskey) {
     }
 }
 
+void connectToMqtt(void) {
+  Serial.println("Connecting to MQTT...");
+  if (!mqttClient.connect()) {
+    reconnectMqtt = true;
+    lastReconnect = millis();
+    Serial.println("Connecting failed.");
+  } else {
+    reconnectMqtt = false;
+  }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(espMqttClientTypes::DisconnectReason reason) {
+  Serial.printf("Disconnected from MQTT: %u.\n", static_cast<uint8_t>(reason));
+
+  if (WiFi.isConnected()) {
+    reconnectMqtt = true;
+    lastReconnect = millis();
+  }
+}
+
+String processor(const String& var){
+//  Serial.println(var);
+  if (var == "PPLUS"){
+    return String(pplus);
+  } else if (var == "PMINUS"){
+    return String(pminus);
+  } else if (var == "QPLUS"){
+    return String(qplus);
+  } else if (var == "QMINUS"){
+    return String(qminus);
+  } else if (var == "APLUS"){
+    return String(aplus);
+  } else if (var == "AMINUS"){
+    return String(aminus);
+  } else if (var == "RPLUS"){
+    return String(rplus);
+  } else if (var == "RMINUS"){
+    return String(rminus);
+  }
+  return String();
+}
+
 void setup() {
 
-    Serial.begin(115200);  
+    Serial.begin(115200);
 
     initSPIFFS();
-
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
 
@@ -351,22 +434,73 @@ void setup() {
     ssid = readFile(SPIFFS, ssidPath);
     pass = readFile(SPIFFS, passPath);
     aeskey = readFile(SPIFFS, aeskeyPath);
+    mqttbroker = readFile(SPIFFS, mqttbrokerPath);
+    String port = readFile(SPIFFS, mqttportPath);
+    if (port) {
+        mqttport = strtol(port.c_str(), 0, 10);
+    }
+    mqtttopic = readFile(SPIFFS, mqtttopicPath);
 
     Serial.println(ssid);
     Serial.println(pass);
     Serial.println(aeskey);
+    Serial.println(mqttbroker);
+    Serial.println(mqttport);
+    Serial.println(mqtttopic);
+
     Set_key(aeskey);
 
     if(initWiFi()) {
+// https://github.com/bertmelis/espMqttClient/blob/main/examples/simple-esp32/simple-esp32.ino
+
+// https://randomnerdtutorials.com/esp32-dht11-dht22-temperature-humidity-web-server-arduino-ide/
+
+        if (mqttbroker && mqttport && mqtttopic) {
+            mqttClient.onConnect(onMqttConnect);
+            mqttClient.onDisconnect(onMqttDisconnect);
+            mqttClient.setServer(mqttbroker.c_str(), mqttport);
+            connectToMqtt();
+        }
+
         // Route for root / web page
         server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-            request->send(SPIFFS, "/index.html", "text/html");
+            request->send(SPIFFS, "/index.html", "text/html", false, processor);
         });
+
         server.serveStatic("/", SPIFFS, "/");
-    
+
+        server.on("/pplus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(pplus));
+        });
+        server.on("/pminus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(pminus));
+        });
+        server.on("/qplus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(qplus));
+        });
+        server.on("/qminus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(qminus));
+        });
+        server.on("/aplus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(aplus));
+        });
+        server.on("/aminus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(aminus));
+        });
+        server.on("/rplus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(rplus));
+        });
+        server.on("/rminus", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "text/plain", String(rminus));
+        });
+
+
         smartmeter_html = readFile(SPIFFS, "/smartmeter.html");
         if (!aeskey.isEmpty()) {
-            smartmeter_html.replace("00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff", aeskey);
+            String str("value=\"");
+            str.concat(aeskey);
+            str.concat("\"");
+            smartmeter_html.replace("value=\"00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff\"", str);
         }
         server.on("/aeskey", HTTP_GET, [](AsyncWebServerRequest *request) {
             request->send(200, "text/html", smartmeter_html);
@@ -375,18 +509,102 @@ void setup() {
             AsyncWebParameter* p = request->getParam(0);
             if(p->isPost()) {
                 Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-
-                // HTTP POST aes-key value
                 if (p->name() == "aes-key") {
-                    aeskey = p->value().c_str();
-                    Serial.print("AES-Key set to: ");
-                    Serial.println(aeskey);
+                    String str = p->value();
+                    Serial.printf("AES-Key set to: %s\r\n", str.c_str());
                     // Write file to save value
-                    writeFile(SPIFFS, aeskeyPath, aeskey.c_str());
+                    writeFile(SPIFFS, aeskeyPath, str.c_str());
                 }
             }
             request->send(200, "text/plain", "Done. ESP will restart...");
-            delay(3000);
+            vTaskDelay(pdMS_TO_TICKS(3000));
+            ESP.restart();
+        });
+
+        mqtt_html = readFile(SPIFFS, "/mqtt.html");
+        if (!mqttbroker.isEmpty()) {
+            String str("value=\"");
+            str.concat(mqttbroker);
+            str.concat("\"");
+            mqtt_html.replace("value=\"broker\"", str);
+        }
+        if (mqttport != 0) {
+            String str("value=\"");
+            str.concat(mqttport);
+            str.concat("\"");
+            mqtt_html.replace("value=\"port\"", str);
+        }
+        if (!mqtttopic.isEmpty()) {
+            String str("value=\"");
+            str.concat(mqtttopic);
+            str.concat("\"");
+            mqtt_html.replace("value=\"topic\"", str);
+        }
+        server.on("/mqtt", HTTP_GET, [](AsyncWebServerRequest *request) {
+            request->send(200, "text/html", mqtt_html);
+        });
+        server.on("/mqtt", HTTP_POST, [](AsyncWebServerRequest *request) {
+            int params = request->params();
+            for (int i = 0; i < params; i++) {
+                AsyncWebParameter* p = request->getParam(i);
+                if (p->isPost()) {
+                    Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+                    if (p->name() == "mqtt-broker") {
+                        String str = p->value();
+                        Serial.printf("mqtt broker set to: %s\r\n", str.c_str());
+                        // Write file to save value
+                        writeFile(SPIFFS, mqttbrokerPath, str.c_str());
+                    } else if (p->name() == "mqtt-port") {
+                        String str = p->value();
+                        Serial.printf("mqtt broker port set to: %s\r\n", str.c_str());
+                        // Write file to save value
+                        writeFile(SPIFFS, mqttportPath, str.c_str());
+                    } else if (p->name() == "mqtt-topic") {
+                        String str = p->value();
+                        Serial.printf("mqtt topic set to: %s\r\n", str.c_str());
+                        // Write file to save value
+                        writeFile(SPIFFS, mqtttopicPath, str.c_str());
+                    }
+                }
+            }
+            request->send(200, "text/plain", "Done. ESP will restart...");
+            vTaskDelay(pdMS_TO_TICKS(3000));
+            ESP.restart();
+        });
+        wlan_html = readFile(SPIFFS, "/wifimanager.html");
+        if (!ssid.isEmpty()) {
+            String str("value=\"");
+            str.concat(ssid);
+            str.concat("\"");
+            wlan_html.replace("value=\"ssid\"", str);
+        }
+        if (!pass.isEmpty()) {
+            String str("value=\"");
+            str.concat(pass);
+            str.concat("\"");
+            wlan_html.replace("value=\"pass\"", str);
+        }
+        server.on("/wlan", HTTP_GET, [](AsyncWebServerRequest *request) {
+            request->send(200, "text/html", wlan_html);
+        });        
+        server.on("/wlan", HTTP_POST, [](AsyncWebServerRequest *request) {
+            int params = request->params();
+            for (int i = 0; i < params; i++) {
+                AsyncWebParameter* p = request->getParam(i);
+                if(p->isPost()) {
+                    if (p->name() == "ssid") {
+                        String str = p->value();
+                        Serial.printf("SSID set to: %s\r\n", str.c_str());
+                        writeFile(SPIFFS, ssidPath, str.c_str());
+                    } else if (p->name() == "pass") {
+                        String str = p->value();
+                        Serial.printf("Password set to: %s\r\n", str.c_str());
+                        writeFile(SPIFFS, passPath, str.c_str());
+                    }
+                }
+            }
+            request->send(200, "text/plain", "Done. ESP will restart...");
+            vTaskDelay(pdMS_TO_TICKS(3000));
             ESP.restart();
         });
 
@@ -411,38 +629,35 @@ void setup() {
     
         server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
             int params = request->params();
-            for (int i=0;i<params;i++) {
+            for (int i = 0; i < params; i++) {
                 AsyncWebParameter* p = request->getParam(i);
                 if(p->isPost()) {
-                    // HTTP POST ssid value
                     if (p->name() == "ssid") {
-                        ssid = p->value().c_str();
-                        Serial.print("SSID set to: ");
-                        Serial.println(ssid);
-                        // Write file to save value
-                        writeFile(SPIFFS, ssidPath, ssid.c_str());
+                        String str = p->value();
+                        Serial.printf("SSID set to: %s\r\n", str.c_str());
+                        writeFile(SPIFFS, ssidPath, str.c_str());
+                    } else if (p->name() == "pass") {
+                        String str = p->value();
+                        Serial.printf("Password set to: %s\r\n", str.c_str());
+                        writeFile(SPIFFS, passPath, str.c_str());
                     }
-                    // HTTP POST pass value
-                    if (p->name() == "pass") {
-                        pass = p->value().c_str();
-                        Serial.print("Password set to: ");
-                        Serial.println(pass);
-                        // Write file to save value
-                        writeFile(SPIFFS, passPath, pass.c_str());
-                    }
-//                    Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
                 }
             }
             request->send(200, "text/plain", "Done. ESP will restart...");
-            delay(3000);
+            vTaskDelay(pdMS_TO_TICKS(3000));
             ESP.restart();
         });
         server.begin();
     }
    
-    xTaskCreate(main_task, "main_task", 4096, 0, 2, NULL);
+    xTaskCreate(usb_task, "usb_task", 4096, 0, 2, NULL);
 }
 
 void loop() {
 
+    static uint32_t currentMillis = millis();
+
+    if (reconnectMqtt && currentMillis - lastReconnect > 5000) {
+        connectToMqtt();
+    }
 }
